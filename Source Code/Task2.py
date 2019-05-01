dda7af14667d3c2f7428b9f4552314aeda19d0c2
saiteja1015 <42659064+saiteja1015@users.noreply.github.com>
2019-04-30 23:09:10 -0500

    Update Task2.py

--- Source Code/Task2.py
+++ Source Code/Task2.py
@@ -1,4 +1,3 @@
-#importing libraries
 import pandas as pd
 import matplotlib.pyplot as plt
 from keras.models import Sequential
@@ -8,23 +7,18 @@ from keras.datasets import mnist
 from keras.utils import np_utils
 from keras.callbacks import TensorBoard
 import tensorflow as tf
-
-#setting batch and epochs
 from sklearn.model_selection import train_test_split
 
 batch_size = 60
 nb_classes = 10
 nb_epoch = 11
 
-#loading the dataset(heart.csv)
 dataset = pd.read_csv("C:/Users/vinee/Downloads/heart.csv", header=None).values
  #print(dataset)
 import numpy as np
 X_train, X_test, Y_train, Y_test = train_test_split(dataset[1:,0:13], dataset[1:,13],
                                                     test_size=0.25, random_state=87)
-#(X_train, y_train), (X_test, y_test) = mnist.load_data()
-# X_train = X_train.reshape(100, 784)
-# X_test = X_test.reshape(5000, 784)
+
 print(X_train)
 X_train = X_train.astype(np.float)
 X_test = X_test.astype(np.float)
@@ -33,7 +27,7 @@ X_test /= 255
 Y_Train = np_utils.to_categorical(Y_train, nb_classes)
 Y_Test = np_utils.to_categorical(Y_test, nb_classes)
 
-#performing Logistic_regression
+#Logistic_regression
 model = Sequential()
 model.add(Dense(output_dim=10, input_shape=(13,), init='normal', activation='softmax'))
 model.compile(optimizer='RMSprop', loss='categorical_crossentropy', metrics=['accuracy'])
@@ -43,13 +37,12 @@ model.summary()
 tensorboard = TensorBoard(log_dir="logslo1/{}",histogram_freq=0, write_graph=True, write_images=True)
 history=model.fit(X_train, Y_Train, nb_epoch=nb_epoch, batch_size=batch_size,callbacks=[tensorboard])
 
-#predicting the accuracy of the model
+#predicting accuracy
 score = model.evaluate(X_test, Y_Test, verbose=1)
 print('Loss: %.2f, Accuracy: %.2f' % (score[0], score[1]))
 
-#plotting the loss
+#plotting loss
 plt.plot(history.history['loss'])
-# plt.plot(history.history['test_loss'])
 plt.title('model loss')
 plt.ylabel('loss')
 plt.xlabel('epoch')
