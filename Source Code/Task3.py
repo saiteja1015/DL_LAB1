607c970bdbee09e8ce8407ff8d73ea85e3de1315
saiteja1015 <42659064+saiteja1015@users.noreply.github.com>
2019-04-30 23:22:16 -0500

    Update Task3.py

--- Source Code/Task3.py
+++ Source Code/Task3.py
@@ -1,32 +1,105 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import cv2
+from tqdm import tqdm
+import os
+
+os.environ['KMP_DUPLICATE_LIB_OK']='True'
+
+DATADIR = "natural_images"
+CATEGORIES = ["airplane","car","cat","dog","flower","fruit","motorbike","person"]
+
+for category in CATEGORIES:
+    path = os.path.join(DATADIR,category)
+    for img in os.listdir(path):
+        img_array = cv2.imread(os.path.join(path,img) ,cv2.IMREAD_GRAYSCALE)
+        plt.imshow(img_array, cmap='gray')
+
+print(img_array.shape)
+
+IMG_SIZE = 100
+
+new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))
+plt.imshow(new_array, cmap='gray')
+plt.show()
+training_data = []
+
+def create_training_data():
+    for category in CATEGORIES:
+        path = os.path.join(DATADIR,category)
+        class_num = CATEGORIES.index(category)
+        for img in tqdm(os.listdir(path)):
+            try:
+                img_array = cv2.imread(os.path.join(path,img) ,cv2.IMREAD_GRAYSCALE)
+                new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))
+                training_data.append([new_array, class_num])
+            except Exception as e:
+                pass
+
+create_training_data()
+print(len(training_data))
 import random
 
-import colours as colours
-import pandas as pd
-import turtle
-import math
+random.shuffle(training_data)
+for sample in training_data[:10]:
+    print(sample[1])
+
+X = []
+y = []
+
+for features,label in training_data:
+    X.append(features)
+    y.append(label)
+
+print(X[0].reshape(-1, IMG_SIZE, IMG_SIZE, 1))
+
+X = np.array(X).reshape(-1, IMG_SIZE, IMG_SIZE, 1)
+
+import pickle
+
+pickle_out = open("X.pickle","wb")
+pickle.dump(X, pickle_out)
+pickle_out.close()
+
+pickle_out = open("y.pickle","wb")
+pickle.dump(y, pickle_out)
+pickle_out.close()
+
+pickle_in = open("X.pickle","rb")
+X = pickle.load(pickle_in)
+
+pickle_in = open("y.pickle","rb")
+y = pickle.load(pickle_in)
 
 
-colours = ["yellow", "gold", "orange", "red", "maroon", "violet", "magenta", "purple", "navy", "blue", "skyblue", "cyan", "turquoise", "lightgreen", "green", "darkgreen", "chocolate", "brown", "black", "gray"]
-dataset_cities = pd.read_csv('cities.csv')
+import tensorflow as tf
+from tensorflow.keras.datasets import cifar10
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten
+from tensorflow.keras.layers import Conv2D, MaxPooling2D
 
-def total_distance(dfcity,path):
-    prev_city = path[0]
-    total_distance = 0
-    step_num = 1
-    for city_num in path[1:]:
-        next_city = city_num
-        total_distance = total_distance + \
-            np.sqrt(pow((dfcity.X[city_num] - dfcity.X[prev_city]),2) + pow((dfcity.Y[city_num] - dfcity.Y[prev_city]),2)) * \
-            (1+ 0.1*((step_num % 10 == 0)*int(not(prime_cities[prev_city]))))
-        prev_city = next_city
-        step_num = step_num + 1
-    return total_distance
+X = X/255.0
+model = Sequential()
 
-dumbest_path = list(dataset_cities.CityId[:].append(pd.Series([0])))
-print('Total distance with the dumbest path is '+ "{:,}".format(total_distance(dataset_cities,dumbest_path)))
+model.add(Conv2D(256, (3, 3), input_shape=X.shape[1:]))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
 
+model.add(Conv2D(256, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
 
+model.add(Flatten())  
 
+model.add(Dense(64))
 
+model.add(Dense(1))
+model.add(Activation('sigmoid'))
 
+model.compile(loss='binary_crossentropy',
+              optimizer='adam',
+              metrics=['accuracy'])
 
+model.fit(X, y, batch_size=32, epochs=3, validation_split=0.3)
